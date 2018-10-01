4987115c5f8b42ddfb903b2af4796f1d85c0aa33
Ryan Vo <rvo@squareup.com>
2018-10-01 16:01:58 -0700

    Add comment

--- addon/services/plaid.js
+++ addon/services/plaid.js
@@ -1,17 +1,16 @@
 import { Promise } from 'rsvp';
 import Service from '@ember/service';
-import { typeOf } from '@ember/utils';
-import { assert } from '@ember/debug';
 
 export default Service.extend({
   isLoading: false,
   isInjected: false,
   plaidPromise: null,
 
-  /**
-    @method injectScript
-    @returns {Promise}
-    @public
+  /** This method loads the Plaid the script. It can be safely called multiple times. If the script
+   *    fails to load and the method is called again, the method will reattempt to load script.
+   * @returns {Promise} a promise which resolves when the Plaid script loads and rejects when it
+   *    does not.
+   * @public
    */
   injectScript() {
     if (this.get('isLoading') || this.get('isInjected')) {
@@ -38,11 +37,12 @@ export default Service.extend({
     }
   },
 
-  /**
-    @method open
-    @param {object} [options] the parameters to pass to Plaid.create
-    @returns {Promise} plaidPromise
-    @public
+  /** This method serves as a proxy to the Plaid link object open method. If it the Plaid script
+   *    isn't loaded, it will try to the script first.
+   * @param {object} [options] the parameters to pass to Plaid.create
+   * @returns {Promise} a promise which resolves when the Plaid script loads and rejects when it
+   *    does not. A resolve does not mean the user has linked their bank account.
+   * @public
    */
   open(options) {
     return this.injectScript().then(() => window.Plaid.create(options).open());
