df7c227de31b5fc031cb9bdb9e36a285c10ebafe
Ryan Vo <rvo@squareup.com>
2018-10-01 16:28:32 -0700

    Remove console statements from dummy app

--- tests/dummy/app/components/dummy-component.js
+++ tests/dummy/app/components/dummy-component.js
@@ -1,24 +1,28 @@
 import Component from '@ember/component';
+import { A } from '@ember/array';
 
 export default Component.extend({
   actions: {
-    onError(err) {
-      console.log(`onError called with err ${JSON.stringify(err, ['message', 'arguments', 'type', 'name'])}`);
-    },
-    onOpen() {
-      console.log('onOpen called');
-    },
-    onLoad() {
-      console.log('onLoad called');
-    },
-    onEvent(eventName, metadata) {
-      console.log(`onEvent called with eventName ${eventName}, metadata ${JSON.stringify(metadata)}`);
-    },
-    onSuccess(token, metadata) {
-      console.log(`onSuccess called with token ${token}, metadata ${JSON.stringify(metadata)}`);
-    },
-    onExit(error, metadata) {
-      console.log(`onExit called with error ${error}, metadata ${JSON.stringify(metadata)}`);
-    },
+    events: A(),
+    actions: {
+      onError(err) {
+        this.get('events').pushObject(`onError called with err ${JSON.stringify(err, ['message', 'arguments', 'type', 'name'])}`);
+      },
+      onOpen() {
+        this.get('events').pushObject('onOpen called');
+      },
+      onLoad() {
+        this.get('events').pushObject('onLoad called');
+      },
+      onEvent(eventName, metadata) {
+        this.get('events').pushObject(`onEvent called with eventName ${eventName}, metadata ${JSON.stringify(metadata)}`);
+      },
+      onSuccess(token, metadata) {
+        this.get('events').pushObject(`onSuccess called with token ${token}, metadata ${JSON.stringify(metadata)}`);
+      },
+      onExit(error, metadata) {
+        this.get('events').pushObject(`onExit called with error ${error}, metadata ${JSON.stringify(metadata)}`);
+      },
+    }
   }
 });
