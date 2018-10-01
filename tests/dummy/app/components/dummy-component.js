d10b67e19108ccc34eb44e13795e3b3e112db9f1
Ryan Vo <rvo@squareup.com>
2018-10-01 13:35:07 -0700

    Update dummy to test service

--- tests/dummy/app/components/dummy-component.js
+++ tests/dummy/app/components/dummy-component.js
@@ -1,19 +1,24 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-
-
-actions: {
-  nextStep() {
-    // intentionally left black
-  },
-
-  openedLink() {
-    // intentionally left black
-  },
-
-  exited() {
-    // intentionally left black
+  actions: {
+    onError(err) {
+      console.log(`onError called with err ${JSON.stringify(err, ['message', 'arguments', 'type', 'name'])}`);
+    },
+    onOpen() {
+      console.log('onOpen called');
+    },
+    onLoad() {
+      console.log('onLoad called');
+    },
+    onEvent(eventName, metadata) {
+      console.log(`onEvent called with eventName ${eventName}, metadata ${JSON.stringify(metadata)}`);
+    },
+    onSuccess(token, metadata) {
+      console.log(`onSuccess called with token ${token}, metadata ${JSON.stringify(metadata)}`);
+    },
+    onExit(error, metadata) {
+      console.log(`onExit called with error ${error}, metadata ${JSON.stringify(metadata)}`);
+    },
   }
-}
-})
+});
