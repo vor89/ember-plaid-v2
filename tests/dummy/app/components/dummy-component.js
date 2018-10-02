1f7ec5c94957f3d3b1fa2f2e49ef750edc19721f
Ryan Vo <rvo@squareup.com>
2018-10-01 17:50:09 -0700

    Fix dummy

--- tests/dummy/app/components/dummy-component.js
+++ tests/dummy/app/components/dummy-component.js
@@ -2,27 +2,25 @@ import Component from '@ember/component';
 import { A } from '@ember/array';
 
 export default Component.extend({
+  events: A(),
   actions: {
-    events: A(),
-    actions: {
-      onError(err) {
-        this.get('events').pushObject(`onError called with err ${JSON.stringify(err, ['message', 'arguments', 'type', 'name'])}`);
-      },
-      onOpen() {
-        this.get('events').pushObject('onOpen called');
-      },
-      onLoad() {
-        this.get('events').pushObject('onLoad called');
-      },
-      onEvent(eventName, metadata) {
-        this.get('events').pushObject(`onEvent called with eventName ${eventName}, metadata ${JSON.stringify(metadata)}`);
-      },
-      onSuccess(token, metadata) {
-        this.get('events').pushObject(`onSuccess called with token ${token}, metadata ${JSON.stringify(metadata)}`);
-      },
-      onExit(error, metadata) {
-        this.get('events').pushObject(`onExit called with error ${error}, metadata ${JSON.stringify(metadata)}`);
-      },
-    }
+    onError(err) {
+      this.get('events').pushObject(`onError called with err ${JSON.stringify(err, ['message', 'arguments', 'type', 'name'])}`);
+    },
+    onOpen() {
+      this.get('events').pushObject('onOpen called');
+    },
+    onLoad() {
+      this.get('events').pushObject('onLoad called');
+    },
+    onEvent(eventName, metadata) {
+      this.get('events').pushObject(`onEvent called with eventName ${eventName}, metadata ${JSON.stringify(metadata)}`);
+    },
+    onSuccess(token, metadata) {
+      this.get('events').pushObject(`onSuccess called with token ${token}, metadata ${JSON.stringify(metadata)}`);
+    },
+    onExit(error, metadata) {
+      this.get('events').pushObject(`onExit called with error ${error}, metadata ${JSON.stringify(metadata)}`);
+    },
   }
 });
